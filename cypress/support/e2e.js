a364f19419d1b77e01e4c7d35f12af50159d48e7
Patrik <patriksusa7@gmail.com>
2026-04-05 19:39:56 +0200

    Full rewrite - stable CI tests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,4 +14,20 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+// Disable all CSS transitions and animations (fix for CI)
+Cypress.on('window:before:load', (win) => {
+  const style = win.document.createElement('style')
+  style.innerHTML = `
+    * {
+      transition: none !important;
+      animation: none !important;
+    }
+    .modal.fade {
+      opacity: 1 !important;
+    }
+    .modal.fade .modal-dialog {
+      transform: none !important;
+    }
+  `
+  win.document.head.appendChild(style)
+})
