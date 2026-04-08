d8f9845085000bc39fec85f4337862aa74a2b64c
Patrik <patriksusa7@gmail.com>
2026-04-08 20:00:50 +0200

    Fix CI: Node 20 + retries config

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.demoblaze.com",
-    viewportWidth: 1280,
-    viewportHeight: 720,
-
-    // RETRIES – stabilné pre CI
     retries: {
-      runMode: 2,   // CI
-      openMode: 0   // lokálne
+      runMode: 2,
+      openMode: 0
     }
-  },
+  }
 });
